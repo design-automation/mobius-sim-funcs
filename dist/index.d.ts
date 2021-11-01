cae758e70e6d646c71621d4381d54c360fb11c3f
phuongtung1 <phuongtung1231992@yahoo.com>
2021-11-01 18:40:03 +0800

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,6 @@
 import * as Funcs from './modules/index';
+import { GIModel as Model } from '@design-automation/mobius-sim/dist/geo-info/GIModel';
+import * as GICommon from '@design-automation/mobius-sim/dist/geo-info/common';
 import { deprecated as _deprecated_funcs } from './deprecated';
 import { _parameterTypes } from './_parameterTypes';
-export { Funcs, _parameterTypes, _deprecated_funcs, };
+export { Funcs, Model, GICommon, _parameterTypes, _deprecated_funcs, };
