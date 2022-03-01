153f01924ddfae6e7b3e0df5041f6b9e58775691
phtj <patrick@janssen.name>
2022-03-01 17:55:32 +0800

    implemented SIM import export

--- src/modules/functions/io/_enum.ts
+++ src/modules/functions/io/_enum.ts
@@ -1,5 +1,6 @@
 export enum _EIOImportDataFormat {
     GI = 'gi',
+    SIM = 'sim',
     OBJ = 'obj',
     GEOJSON = 'geojson',
     CITYJSON = 'CityJSON'
@@ -14,6 +15,7 @@ export enum _EIODataTarget {
 }
 export enum _EIOExportDataFormat {
     GI = 'gi',
+    SIM = 'sim',
     OBJ_VERT = 'obj_v',
     OBJ_POSI = 'obj_ps',
     // DAE = 'dae',
