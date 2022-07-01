ea744cda140b861c196f30a50aa27c685c8493d3
Sheu Wen <sheuwen251@gmail.com>
2022-07-01 17:13:48 +0800

    Documentation examples bugfix

--- example/modify.offset.js
+++ example/modify.offset.js
@@ -11,7 +11,7 @@ function modifyOffset() {
     const c = Funcs.make.Position([8, 6, 18]) 
     const d = Funcs.make.Position([11, 0, 22]) 
     const pgon1 = Funcs.make.Polygon([a,b,d,c]) // creating a pgon
-    const pgon2 = Funcs.make.copy(pgon1, null)
+    const pgon2 = Funcs.make.Copy(pgon1, null)
     Funcs.modify.Offset(pgon2, 3)
     return Funcs.io.ExportData(null, 'gi'); // return the export file as a string
 }
